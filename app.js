0679e082d71ec5d9624f84679b2beea44d42e76c
Michal Mottl <michal.mottl@coleurope.eu>
2018-05-10 12:37:40 +0700

    update d3 svg join

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 var minYear = birthData[0].year;
 var maxYear = birthData[birthData.length - 1].year;
 
+var width = 600;
+var height = 600;
+var barWidth = 25'
+
 d3.select("input")
     .property("min", minYear)
     .property("max", maxYear)
     .property("value", minYear);
+
+d3.select("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .selectAll("rect")
+  .data(birthData.filter(function(d){
+      return d.year === minYear;
+  }))
+  .enter()
+  .append("rect")
+    .attr("width", barWidth)
+    .attr("height", height)
+    .attr("y", 0)
+    .attr("x", function(d, i){
+        return barWidth*i;
+    })
