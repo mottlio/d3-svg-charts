77fe8dafad008dc96063749c75e5a435ae412ed7
Michal Mottl <michal.mottl@coleurope.eu>
2018-05-10 13:11:49 +0700

    connect the slider input, d3 and SVGs

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var maxYear = birthData[birthData.length - 1].year;
 
 var width = 600;
 var height = 600;
-var barWidth = 25'
+var numBars = 12;
+var barPadding = 10;
+var barWidth = width / numBars - barPadding;
 
 d3.select("input")
     .property("min", minYear)
@@ -16,12 +18,32 @@ d3.select("svg")
   .selectAll("rect")
   .data(birthData.filter(function(d){
       return d.year === minYear;
-  }))
+    }))
   .enter()
   .append("rect")
     .attr("width", barWidth)
-    .attr("height", height)
-    .attr("y", 0)
+    .attr("height", function(d){
+        return d.births/ 2.5e6 * height;
+    })
+    .attr("y", function(d){
+        return height - d.births/ 2.5e6 * height;
+    })
     .attr("x", function(d, i){
-        return barWidth*i;
+        return (barWidth + barPadding)*i;
+    })
+    .attr("fill", "purple")
+
+d3.select("input")
+    .on("input", function(){
+        var year = +d3.event.target.value;
+        d3.selectAll("rect")
+            .data(birthData.filter(function(d) {
+                return d.year === year;
+            }))
+            .attr("height", function(d){
+                return d.births / 2.5e6 * height;
+            })
+            .attr("y", function(d){
+                return height - d.births / 2.5e6 * height;
+            })
     })
